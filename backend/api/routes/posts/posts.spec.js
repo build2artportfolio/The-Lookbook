9c6e4274d44b39233f9d9c73c15cd09313293c3c
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-11 13:58:42 -0700

    Created test cases for a /api/posts POST route.

--- backend/api/routes/posts/posts.spec.js
+++ backend/api/routes/posts/posts.spec.js
@@ -3,6 +3,7 @@ const server = require("../../server");
 const db = require("../../../data/database");
 const Users = require("../../../data/models/usersModel");
 const Posts = require("../../../data/models/postsModel");
+const jwt = require("jsonwebtoken");
 const faker = require("faker");
 //After each test, reset the users table.
 afterEach(async () => {
@@ -11,7 +12,7 @@ afterEach(async () => {
 });
 
 describe("Post Routes", () => {
-	describe("/", () => {
+	describe("GET /", () => {
 		it("should return status 200", async () => {
 			const res = await request(server).get("/api/posts");
 			expect(res.status).toBe(200);
@@ -48,6 +49,58 @@ describe("Post Routes", () => {
 				.set("page", "4");
 			expect(res.body.totalPosts).toBe(100);
 			expect(res.body.posts.length).toBe(17);
+		});
+	});
+
+	describe("POST /", () => {
+		it("should return status 401 if no JWT token in headers", async () => {
+			let newPost = {
+				title: faker.lorem.words(),
+				description: faker.lorem.sentence(),
+				image: faker.image.imageUrl()
+			};
+			const res = await request(server)
+				.post("/api/posts")
+				.send(newPost);
+			expect(res.status).toBe(401);
+		});
+
+		it("should return status 422 if no title or image provided", async () => {
+			let newPost = {
+				title: faker.lorem.words(),
+				description: faker.lorem.sentence()
+			};
+			const res = await request(server)
+				.post("/api/posts")
+				.send(newPost);
+			expect(res.status).toBe(422);
+		});
+
+		it("should return status 201 and the created post with successful authentication", async () => {
+			//Create user. Users model is already tested, so we are just using it to provide a valid Foreign Key to the created posts.
+			const user = await Users.create({
+				username: "Michael",
+				password: "test"
+			});
+			//Now create a JWT for the users authentication, just for testing the API's response.
+			const payload = {
+				id: user.id,
+				username: user.username
+			};
+			const token = await jwt.sign(payload, process.env.JWT_SECRET, {
+				expiresIn: "1d"
+			});
+			//
+			let newPost = {
+				title: faker.lorem.words(),
+				description: faker.lorem.sentence(),
+				image: faker.image.imageUrl()
+			};
+			const res = await request(server)
+				.post("/api/posts")
+				.send(newPost)
+				.set("authorization", token);
+			expect(res.status).toBe(422);
 		});
 	});
 });
