ad76f8f38635408215ff19f613b1156fbd18b543
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-11 11:03:12 -0700

    Created create() method in postsModel, passing all tests.

--- backend/data/models/postsModel/index.js
+++ backend/data/models/postsModel/index.js
@@ -1,3 +1,21 @@
 const db = require("../../database");
 
-module.exports = {};
+const create = async post => {
+	//destructure returned ID into a single variable.
+	const [createdPostId] = await db("posts").insert(post, "id");
+	//Return created post with user property of who created it as well.
+	const createdPost = await db("posts")
+		.where({ id: createdPostId })
+		.first();
+	const artist = await db("users")
+		.select("id", "username")
+		.where({ id: createdPost.artistId })
+		.first();
+	createdPost.artist = artist;
+	delete createdPost.artistId;
+	return createdPost;
+};
+
+module.exports = {
+	create
+};
--- backend/data/models/postsModel/postsModel.spec.js
+++ backend/data/models/postsModel/postsModel.spec.js
@@ -1,31 +1,51 @@
 const db = require("../../database");
 const Posts = require("./index");
+const Users = require("../usersModel");
 const faker = require("faker");
 
 describe("Posts Model Functions", () => {
-	afterEach(() => {
-		db("posts").truncate();
+	afterEach(async () => {
+		await db("posts").del();
+		await db("users").del();
 	});
 	describe("create()", () => {
 		it("should create a post in the database based on the provided post object", async () => {
+			//Create user. Users model is already tested, so we are just using it to provide a valid Foreign Key to the created post.
+			const user = await Users.create({
+				username: "Michael",
+				password: "test"
+			});
 			const postObject = {
 				title: faker.lorem.words(),
 				description: faker.lorem.sentence(),
 				imageUrl: faker.image.imageUrl(),
-				artistId: 1
+				artistId: user.id
 			};
 			const createdPost = await Posts.create(postObject);
+			//We don't expect the artistId back, as we will get an artist object in place of it.
+			//Because of this, we delete it.
+			delete postObject.artistId;
+			delete createdPost.artistId;
 			//Expect the created post to have all the properties of the postObject.
 			expect(createdPost).toEqual({ ...createdPost, ...postObject });
 		});
 
 		it("should create a post in the database even if description is not provided", async () => {
+			//Create user. Users model is already tested, so we are just using it to provide a valid Foreign Key to the created post.
+			const user = await Users.create({
+				username: "Michael",
+				password: "test"
+			});
 			const postObject = {
 				title: faker.lorem.words(),
 				imageUrl: faker.image.imageUrl(),
-				artistId: 1
+				artistId: user.id
 			};
 			const createdPost = await Posts.create(postObject);
+			//We don't expect the artistId back, as we will get an artist object in place of it.
+			//Because of this, we delete it.
+			delete postObject.artistId;
+			delete createdPost.artistId;
 			//Expect the created post to have all the properties of the postObject.
 			expect(createdPost).toEqual({ ...createdPost, ...postObject });
 		});
