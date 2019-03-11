4512ca6e6067a8e2c5a6bef534cc40b1e366ff5e
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-10 21:59:48 -0700

    Created test cases for Users Model getOne() function.

--- backend/data/models/usersModel/usersModel.spec.js
+++ backend/data/models/usersModel/usersModel.spec.js
@@ -19,4 +19,34 @@ describe("Users Model Functions", () => {
 			expect(createdUser).toEqual({ ...createdUser, ...newUser });
 		});
 	});
+
+	describe("getOne()", () => {
+		it("fetches a user by the filter provided", async () => {
+			//Create a user in the Database, using our already tested user create method.
+			//This way we can test the getOne() method with the created user's properties.
+			const newUser = {
+				username: "Michael",
+				password: "testpass"
+			};
+			const createdUser = await Users.create(newUser);
+			//Here we test if the filter passed into our getOne method works for one, or multiple properties.
+			const fetchById = await Users.getOne({ id: createdUser.id });
+			expect(fetchById).toEqual(createdUser);
+			const fetchByUsername = await Users.getOne({
+				username: createdUser.username
+			});
+			expect(fetchByUsername).toEqual(createdUser);
+			const fetchByIdAndUsername = await Users.getOne({
+				id: createdUser.id,
+				username: createdUser.username
+			});
+			expect(fetchByIdAndUsername).toEqual(createdUser);
+		});
+
+		it("returns null if no user is found", async () => {
+			//Our database starts ID incrementation at 1, so we know no record will ever have the Id of 0.
+			const fetchNoUser = await Users.getOne({ id: 0 });
+			expect(fetchNoUser).toBe(null);
+		});
+	});
 });
