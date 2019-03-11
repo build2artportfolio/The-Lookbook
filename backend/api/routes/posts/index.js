d42a14ee834355b4c399b43676047e20eff1acfb
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-11 12:21:31 -0700

    Created postRoutes and a spec file for testing the postRoutes.

---
+++ backend/api/routes/posts/index.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const Posts = require("../../../data/models/postsModel");
+
+module.exports = router;
---
+++ backend/api/routes/posts/posts.spec.js
@@ -0,0 +1,19 @@
+const request = require("supertest");
+const server = require("../../server");
+const db = require("../../../data/database");
+const Users = require("../../../data/models/usersModel");
+const Posts = require("../../../data/models/postsModel");
+
+//After each test, reset the users table.
+afterEach(async () => {
+	await db("posts").del();
+});
+
+describe("Post Routes", () => {
+	describe("/", () => {
+		it("should return status 200", async () => {
+			const res = await request(server).get("/api/posts");
+			expect(res.status).toBe(200);
+		});
+	});
+});
