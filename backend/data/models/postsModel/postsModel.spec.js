a7552544f0283895e1ac7dfa56e77165e609f970
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-11 11:13:53 -0700

    Created test cases for the postsModel getOne() method.

--- backend/data/models/postsModel/postsModel.spec.js
+++ backend/data/models/postsModel/postsModel.spec.js
@@ -50,4 +50,39 @@ describe("Posts Model Functions", () => {
 			expect(createdPost).toEqual({ ...createdPost, ...postObject });
 		});
 	});
+
+	describe("getOne()", () => {
+		it("should return one Post based on the filter properties provided", async () => {
+			//Create a post for us to fetch first.
+			//Create user. Users model is already tested, so we are just using it to provide a valid Foreign Key to the created post.
+			const user = await Users.create({
+				username: "Michael",
+				password: "test"
+			});
+			const postObject = {
+				title: faker.lorem.words(),
+				imageUrl: faker.image.imageUrl(),
+				artistId: user.id
+			};
+			const createdPost = await Posts.create(postObject);
+			//We don't expect the artistId property, as we get an artist object instead. So we delete the property from this so it's not expected.
+			delete createdPost.artistId;
+			//Now we test the getOne() method.
+			const getPost = await Posts.getOne({ id: createdPost.id });
+			const expected = {
+				...createdPost,
+				artist: {
+					id: user.id,
+					username: user.username
+				}
+			};
+			expect(getPost).toEqual(expected);
+		});
+
+		it("should return null if no Post found", async () => {
+			//Our Post ID incrementation starts at 1, so we know a post with ID 0 won't ever be present.
+			const foundPost = await Posts.getOne({ id: 0 });
+			expect(foundPost).toBe(null);
+		});
+	});
 });
