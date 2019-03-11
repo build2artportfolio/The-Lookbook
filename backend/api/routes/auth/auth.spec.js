f0f73635931d19348f78455cc50676b2b9c2320e
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-10 23:16:59 -0700

    Added another test case to the /api/auth/register route to make sure it responds with the correct status code and message when credentials on a new register are already taken.

--- backend/api/routes/auth/auth.spec.js
+++ backend/api/routes/auth/auth.spec.js
@@ -44,5 +44,26 @@ describe("Auth Routes", () => {
 				.set("Accept", "application/json");
 			expect(res.body).toEqual({ message: "Successfully registered account." });
 		});
+
+		it("should return Status 400 and a message saying credentials are already in use.", async () => {
+			const body = {
+				username: "Michael",
+				password: "MyPassWordHere"
+			};
+			//Have api create one account.
+			const res1 = await request(server)
+				.post("/api/auth/register")
+				.send(body)
+				.set("Accept", "application/json");
+			//Attempt to create another account with the same credentials. Should not allow us to, as the username is already in use.
+			const res2 = await request(server)
+				.post("/api/auth/register")
+				.send(body)
+				.set("Accept", "application/json");
+			expect(res2.status).toBe(400);
+			expect(res2.body).toEqual({
+				message: "Account with those credentials is already taken."
+			});
+		});
 	});
 });
