bfcc620f73b9436f09ce6213db7179752fbf7fb7
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-12 09:53:19 -0700

    Created test case for Posts Model getByArtistId

--- backend/data/models/postsModel/postsModel.spec.js
+++ backend/data/models/postsModel/postsModel.spec.js
@@ -109,6 +109,24 @@ describe("Posts Model Functions", () => {
 		});
 	});
 
+	describe("getByArtist()", () => {
+		it("should return posts only by the artist", async () => {
+			//Create user. Users model is already tested, so we are just using it to provide a valid Foreign Key to the created post.
+			const user = await Users.create({
+				username: "Michael",
+				password: "test"
+			});
+			//Generate posts with fakerJS
+			const genPosts = await generatePosts(100, user.id);
+			//insert generated posts
+			await db("posts").insert(genPosts);
+			//Execute Posts.get with pagination values
+			const fetchedPosts = await Posts.getByArtistId(user.id);
+			//Test if the properties are accurate compared to the posts we generated.
+			expect(fetchedPosts.length).toBe(100);
+		});
+	});
+
 	describe("update()", () => {
 		it("should return the updated record based on the properties to update passed to it.", async () => {
 			//Create user. Users model is already tested, so we are just using it to provide a valid Foreign Key to the created post.
