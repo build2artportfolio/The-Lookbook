8cac0a548d64ec1dc8ad5cfe17ac3562f999c715
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-12 10:05:23 -0700

    Created test cases for /api/users/:id GET route.

--- backend/api/routes/users/users.spec.js
+++ backend/api/routes/users/users.spec.js
@@ -11,6 +11,20 @@ afterEach(async () => {
 
 describe("Users Routes", () => {
 	describe("GET /:id", () => {
-		it("should return status code 200", () => {});
+		it("should return status code 200", async () => {
+			//Create user. Users model is already tested, so we are just using it to provide a valid user to get results from.
+			const user = await Users.create({
+				username: "Michael",
+				password: "test"
+			});
+			const res = await request(server).get(`/api/users/${user.id}`);
+			expect(res.status).toBe(200);
+		});
+
+		it("should return 404 if no user found", async () => {
+			//Incrementation of ID's start at 1, so we know there is no user with ID of 0.
+			const res = await request(server).get(`/api/users/0`);
+			expect(res.status).toBe(404);
+		});
 	});
 });
