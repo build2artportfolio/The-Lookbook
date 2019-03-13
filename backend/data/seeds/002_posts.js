d6742d20d849627ab8ff058f277fcc38d89b8696
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-12 17:45:54 -0700

    Removed static ID's from posts seeding.

--- backend/data/seeds/002_posts.js
+++ backend/data/seeds/002_posts.js
@@ -3,21 +3,18 @@ const faker = require("faker");
 const generatePosts = async () => {
 	// Inserts seed entries
 	let postSeeds = [];
-	let currentPostId = 1;
 
 	//Here we run a for loop in another. This creates 100 post seeds, for 5 users each.
 	//We do it this way so the posts creation dates aren't in order of user. Makes the data more realistic.
 	for (let i = 1; i <= 5; i++) {
 		for (let k = 1; k <= 5; k++) {
 			let newSeed = {
-				id: currentPostId,
 				title: faker.lorem.words(),
 				description: faker.lorem.sentence(),
 				imageUrl: "https://picsum.photos/300/300/?random",
 				artistId: k
 			};
 			postSeeds.push(newSeed);
-			currentPostId += 1;
 			if (i === 5 && k === 5) return postSeeds;
 		}
 	}
