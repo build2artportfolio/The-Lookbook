b29465c7ed438f8c7ff928851d1e43e782d9fc0a
mrsteveson <patrick.steveson727@gmail.com>
2019-03-13 18:00:43 -0500

    deleted the redirecting alert listener

--- js/index.js
+++ js/index.js
@@ -11,11 +11,11 @@ for (let i = 0; i < aTag.length; i++) {
 
 //Click Event
 // Prompts an alert saying Redirecting when a navigation is clicked.
-let navPro = document.querySelector("nav");
+// let navPro = document.querySelector("nav");
 
-navPro.addEventListener("click", function() {
-  alert("Redirecting");
-});
+// navPro.addEventListener("click", function() {
+//   alert("Redirecting");
+// });
 
 // Click Event Listener
 // Changes any Ptag section's text to bold.
